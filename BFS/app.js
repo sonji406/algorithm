cffa4ecc6d8cc0cfa449673734240800d6a98809
sonjieun <wkaqh24@naver.com>
2024-09-11 00:11:15 +0900

    Set: 파일 세팅

---
+++ BFS/app.js
@@ -0,0 +1,18 @@
+const array = require('fs').readFileSync('./input.txt').toString().split('\n');
+
+console.log(array);
+
+// input = input.split('\n');
+
+// const testCaseNum = Number(input[0]);
+
+// for (let i = 0; i <= testCaseNum; i++) {
+//     const arr = input[i].split(' ');
+//     let newArr = [];
+
+//     for (let i = 0; i < arr.length; i++) {
+//         const arr = array[i];
+//     }
+
+//     break;
+// }
